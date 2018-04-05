32bdb19755bed6c4c61bcb4f1a66a42a1410b288
David Kenney <kenneyd@users.noreply.github.com>
2018-04-05 17:50:32 +0900

    Remove pipenv stuff (#17)
    * remove pipfile and pipfile.lock
    
    * Update setup.py with dependencies
    
    * remove duplicate install param in setup.py

--- setup.py
+++ setup.py
@@ -4,4 +4,18 @@ Installs flatlands, a gym-compatible driving-on-track simulator
 
 from setuptools import setup
 
-setup(name='gym_flatlands', version='0.0.1', install_requires=['gym'])
+setup(name='gym_flatlands',
+      install_requires=[
+          'gym',
+          'scipy',
+          'mpi4py',
+          'cloudpickle',
+          'tensorflow',
+          'pygame',
+          'pyproj'
+      ],
+      description='Flatlands simple on-track driving simulator with physics',
+      author='AscentAI',
+      url='https://github.com/ascentai/flatlands-gym',
+      author_email='david@ascent.ai',
+      version='0.1.2')
