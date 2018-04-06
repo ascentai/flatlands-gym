11a98964aae557118d4368650fc860534a7ee162
David Kenney <david@ascent.ai>
2018-04-06 17:56:53 +0900

    Add docstring and reward threshold to registration

--- gym_flatlands/__init__.py
+++ gym_flatlands/__init__.py
@@ -1,6 +1,11 @@
+"""
+Responsible for adding our environment to the Gym registry
+Based off of https://github.com/openai/gym/blob/master/gym/envs/__init__.py
+"""
 from gym.envs.registration import register
 
 register(
     id='flatlands-v0',
     entry_point='gym_flatlands.envs:FlatlandsEnv',
+    reward_threshold=1000
 )
