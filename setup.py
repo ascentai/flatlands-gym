b56065b417f799dd1888c56e604332d42160c69e
David Kenney <david@ascent.ai>
2018-04-11 11:13:42 +0900

    update the setup.py installation file so that it registers package and data files properly

--- setup.py
+++ setup.py
@@ -5,25 +5,20 @@ Installs flatlands, a gym-compatible driving-on-track simulator
 from os import path
 from setuptools import setup
 
-here = path.abspath(path.dirname(__file__))
-
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-    long_description = f.read()
+HERE = path.abspath(path.dirname(__file__))
+with open(path.join(HERE, 'README.md'), encoding='utf-8') as f:
+    LONG_DESC = f.read()
 
 setup(
     name='flatlands',
     install_requires=[
         'gym',
         'scipy',
-        'mpi4py',
-        'cloudpickle',
-        'tensorflow',
         'pyproj',
-        'pygame',
     ],
     description='Flatlands simple on-track driving simulator with physics',
-    long_description=long_description,
+    long_description=LONG_DESC,
     long_description_content_type='text/markdown',
     author='AscentAI',
     url='https://github.com/ascentai/flatlands-gym',
@@ -36,5 +31,6 @@ setup(
     keywords='driving simulation gym',  # Optional,
     license='MIT',
     python_requires='>=3',
-    data_files=[('my_data', ['gym_flatlands/envs/flatlands_sim/original_circuit_green.csv'])],
+    data_files=[('map', ['flatlands/envs/flatlands_sim/original_circuit_green.csv'])],
+    packages=['flatlands'],
 )
